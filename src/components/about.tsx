dfd5f984e8b5650ec37a226556032c67be9f3a0f
Wajahat Ali Khan <assim345678@gmail.com>
2025-01-13 23:56:53 +0500

    Update about.tsx

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -6,7 +6,8 @@ const About = () => {
         <h2 data-aos="zoom-in-down"  className='text-3xl md:text-5xl'>About Me</h2>
         <p className='text-white pt-4 mx-6 md:mx-32' >I am a student in GIAIC (Governor Initiative for Artificial Intelligence and Computing) persuing a course in Artificial Intelligence, Web 3.0 and Metaverse. Passionate about Technology and constantly learning new skills to stay up-to-date with latest innovations.
             <br />
-            I have complete my basic web Development (Html, CSS and JavaScript) course from BanoQabil 2.0
+          I have completed my Basic Frontend Web Development course (HTML, CSS, and JavaScript), building a strong foundation in web technologies.
+
         </p>
 
     </div>
