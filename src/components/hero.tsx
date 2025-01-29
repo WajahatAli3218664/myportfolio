bb2a854e86710288d6d713d405bab67798cb8fa6
Wajahat Ali Khan <assim345678@gmail.com>
2025-01-30 01:56:14 +0500

    Update hero.tsx

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <div id="hero" >
             <Navbar />
             <div className='container grid lg:grid-cols-2 h-auto lg:h-[calc(100vh-60px)] items-center mx-auto md:mb-10 sm:mb-10'>
-                <div data-aos="zoom-in-down" className='mx-auto'> <Image src="wajo.jpg" width={550} height={550} alt="Profile picture" /></div>
+                <div data-aos="zoom-in-down" className='mx-auto'> <Image src="waj.jpg" width={550} height={550} alt="Profile picture" /></div>
                 <div className='text-center lg:text-left sm:text-[40px] lg:text-[100px] leading-tight '>
                     <div className='ml-[5%]'>
                         <p className='text-[40px]'>Hi! My name is</p>
